a4d543775568a2277ebd35e913de1f6b94ca7d5e
Juan Cruz Vinderola <juancruzrcv@gmail.com>
2023-05-20 21:47:00 -0300

    Boton de wsp + estructura y estilos zona contactos

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton-wsp").addEventListener("click", function() {
+  window.open("https://api.whatsapp.com/send?phone=+543412128692&text=hola, soy juan", "_blank");
+});
+
