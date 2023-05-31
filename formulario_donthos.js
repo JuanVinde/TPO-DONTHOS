a97fb2f50209fd16990010c968a8f303423f5be6
Juan Cruz Vinderola <juancruzrcv@gmail.com>
2023-05-31 02:24:41 -0300

    Cambios en la seccion de coberturas

--- formulario_donthos.js
+++ formulario_donthos.js
@@ -3,7 +3,7 @@ let footerjs= `
 
                 
                 
-<a href="index.html"><img src="fondo_blanco.jpeg" alt="" class="img-footer"> </a>
+<a href="index.html"><img src="/img/logo-final.png" alt="Logo" class="img-footer"> </a>
 
 <p>copyrith@2023 Donthos</p>
 
@@ -12,25 +12,25 @@ let footerjs= `
 <div class="contenedor-redes-footer">
 
 <a href="https://www.facebook.com/profile.php?id=100064057568624" target="_blank" class="facebook-f">
-    <img src="facebook.png" alt="facebook" class="fb">
+    <img src="/img/facebook-blanco (1).png" alt="link a facebook" class="fb">
 </a>
-<a href="*" target="_blank" class="instagram-f">
-    <img src="instagram.png" alt="" class="insta">
+<a href="https://www.instagram.com/" target="_blank" class="instagram-f">
+    <img src="/img/instagram-blanco.png" alt="link a instagram" class="insta">
 </a>
-<a href="*" target="_blank" class="whatsapp-f">
-    <img src="whatsapp.png" alt="" class="wsp">
+<a href="https://www.whatsapp.com/?lang=es" target="_blank" class="whatsapp-f">
+    <img src="/img/whatsapp-blanco.png" alt="link a whatsapp" class="wsp">
 </a>
 <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKkxGsLHzRrgKDPVhnRsdGvRfMcZhVTsQtBLZXDqmCjMvrGlCdslrQPcmJLLDCMTsBjVxWR" target="_blank" class="gmail-f">
-    <img src="gmail.png" alt="" class="mail">
+    <img src="/img/gmail-blanco.png" alt="link a gmail" class="mail">
 </a>
 </div>
 
 <div class="contenedor-grupo">
 <h5>Pagina creada por:</h5>
 <ul>
-    <li>- Juan Cruz, Vinderola Breppe</li>
-    <li>- Gabriel Lombardi</li>
-    <li>- sebastian</li>
+    <li>Juan Cruz, Vinderola Breppe</li>
+    <li>Gabriel, Lombardi</li>
+    <li>Sebastian</li>
 </ul>
 </div>`
 
